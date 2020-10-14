e40a71f0421500672618d909cef84abd3f06bb86
ThenoobMario <soni.nachiket@yahoo.in>
2020-10-14 19:45:14 +0530

    Formatting

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 // NodeJS is an open source run time environment that executes JavaScript on the server side
-let express = require("express") // Import express module and give our program access to it
-let app = express() // Create an express application and assign it to the app variable
+const express = require("express") // Import express module and give our program access to it
+const app = express() // Create an express application and assign it to the app variable
+const port = process.env.PORT || 81
 // An express app is a series of calls to functions called as middleware functions
 // These functions have access to HTTP Requests and Response objects as well as the next function for applications request and response cycle
 
-app.use(function (req,res,next) {
+app.use((req,res,next) => {
     // When a request comes in:
     // 1) Loggin information about the request to the terminal about where the express server is running
     console.log(`${new Date()} - ${req.method} request for ${req.url}`);
@@ -14,6 +15,6 @@ app.use(function (req,res,next) {
 // The next handler responds by serving any static files inside the given directory
 app.use(express.static("../static"));
 // Specify the port at which Express should listen on
-app.listen(81,function(){
-    console.log("Serving static on port 81")
+app.listen(port, () => {
+    console.log('Serving static on port '+ port)
 })
--- static/predict.js
+++ static/predict.js
@@ -1,15 +1,15 @@
 // When an image file is selected with the image-selector, trigger the following change
-$("#image-selector").change(function(){
+$("#image-selector").change(() => {
     // Create a File Reader Object to allow the web app to read the contents of the selected file
-    let reader = new FileReader(); 
+    let reader = new FileReader();
     // Set the onload handler for reader which is triggered once the contents of the file is successfully read
-    reader.onload = function(){ 
+    reader.onload = () => {
         // Intiailize the dataURL variable to contain the image data as an URL which represents the file's data as base64 encoded string
         let dataURL = reader.result;
         // Set the source attribute of the selected image to the value of dataURL
-        $(`#selected-image`).attr("src",dataURL); 
+        $(`#selected-image`).attr("src",dataURL);
         // Get rid of any previous predictions
-        $("#prediction-list").empty(); 
+        $("#prediction-list").empty();
     }
     // Get the image file from the image selector
     let file =  $("#image-selector").prop('files')[0];
@@ -17,40 +17,42 @@ $("#image-selector").change(function(){
     reader.readAsDataURL(file);
 })
 
-let model;
+const model;
 // Immediately Invoked Function Express
-(async function(){
+(async function getModel(){
     // Load our tensorflow.js file
-    model = await tf.loadModel('./model/model.json');
+    model = await tf.loadLayersModel('https://github.com/ThenoobMario/tfjs_practice/blob/main/model.json');
     $('.progress-bar').hide();
-})();
+
+    console.log('Model loaded');
+});
 
 // When the user clicks the predict button
-$("preduct-button").click(async function(){
+$("predict-button").click(async function(){
     // Get the image from the select image element
-    let image = $('#selected-image').get(0);
+    let image = $('#selected-image').get();
     // Create a tensor object from the image
     // Resize it to 224X224
     // Cast the type to float32
     // Expand the dimensions to be of rank 4
-    let tensor = tf.fromPixeks(image).resizeNearestNeighbor([224,224]).toFloat().expandDims();
+    let img_tensor = tf.fromPixels(image).resizeNearestNeighbor([224,224]).cast(img_tensor, 'float32').expandDims();
 
     // Pre-processing
 
     // Calling predict on our model and passing the tensor to it
     // Returns tensor of the output predictions for the given input
     let predictions = await model.predict(tensor).data();
-    let top5 = Array.from(predictions).map(function(p,i){
+    let top5 = Array.from(predictions).map((p, i) => {
         return {
             probability: p,
-            className = IMAGENET_CLASSES[i]
+            className: IMAGENET_CLASSES[i]
         };
-    }).sort(function (a,b){
+    }).sort((a,b) => {
         return b.probability - a.probability;
     }).slice(0,5);
 
     $("#prediction-list").empty();
-    top5.forEach(function(p){
+    top5.forEach((p) => {
         $('#prediction-list').append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
     });
 
